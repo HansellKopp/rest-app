7e5c63742bdac081103bb9695655c45129256512
Hansell Kopp <sistemafrenos@gmail.com>
2023-08-30 10:42:56 +0200

    Add validators

---
+++ Api/Features/Products/Validators/CategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Api.Features.Products.Dtos;
+
+
+namespace Api.Features.Products.Validators
+{
+	public class CategoryValidator : AbstractValidator<CategoryDTO>
+	{
+		public CategoryValidator()
+		{
+			RuleFor(p => p.Name).MaximumLength(2).NotEmpty();
+		}
+	}
+}
+
---
+++ Api/Features/Products/Validators/ProductValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Api.Features.Products.Dtos;
+
+namespace Api.Features.Products.Validators
+{
+	public class ProductValidator : AbstractValidator<ProductDTO>
+    {
+		public ProductValidator()
+		{
+			RuleFor(p => p.Category).NotEmpty();
+			RuleFor(p => p.Name).MinimumLength(2).NotEmpty();
+		}
+	}
+}
+
