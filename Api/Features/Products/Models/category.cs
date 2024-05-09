9073d084c1c2e2e810215ef7121deeb8eea2579c
Hansell Kopp <hansell.kopp@frontend-gmbh.de>
2024-05-09 17:52:49 +0200

    Dependencies update

--- Api/Features/Products/Models/category.cs
+++ Api/Features/Products/Models/category.cs
@@ -1,12 +1,11 @@
 using System.Text.Json.Serialization;
 using Api.Models;
+using Microsoft.Extensions.Hosting;
 
 namespace Api.Features.Products.Models;
 public class Category : BaseEntity
 {
     public required string Name { get; set; }
     public string? Image { get; set; }
-
-    [JsonIgnore]
-    public virtual ICollection<Product>? Products { get; set; }
+    public ICollection<Product> Products { get; } = new List<Product>();
 }
